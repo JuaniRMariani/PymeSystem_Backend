b8e9a37214daeb4ff4a70bad763924b2bf83c8e4
Juan Ignacio <juanirmariani@gmail.com>
2025-02-07 12:03:29 -0300

    First Commit

---
+++ src/main/java/com/example/pymesystem_backend/PymeSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pymesystem_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PymeSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PymeSystemBackendApplication.class, args);
+    }
+
+}
